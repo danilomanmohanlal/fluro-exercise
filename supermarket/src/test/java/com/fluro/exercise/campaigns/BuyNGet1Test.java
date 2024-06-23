0e6f7a99a2691e49c5aaece1759fa89fe48cf87f
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2024-06-23 12:20:05 +0100

    Add test case scenario for buy n get 1

--- supermarket/src/test/java/com/fluro/exercise/campaigns/BuyNGet1Test.java
+++ supermarket/src/test/java/com/fluro/exercise/campaigns/BuyNGet1Test.java
@@ -49,4 +49,28 @@ class BuyNGet1Test {
 
     }
 
+    @Test
+    public void test_CalculateBuyNGet1_MultipleApplied() {
+
+        Map<String, Integer> cart = new HashMap<>();
+        int productQty = 9;
+        cart.put("A", 1);
+        cart.put("B", 2);
+        cart.put("C", productQty);
+        cart.put("D", 1);
+        cart.put("E", 1);
+
+        int quantity = 3;
+        int toSubtract = productQty / (quantity + 1);
+
+        BuyNGet1 buyNGet1 = new BuyNGet1("C", quantity);
+
+        //this campaign always returns 0, we need to check the cart
+        buyNGet1.calculateDiscount(cart);
+
+        assertTrue(cart.containsKey("C"));
+        assertEquals(productQty - toSubtract, cart.get("C"));
+
+    }
+
 }
