0d7e6c872d249682527d29042155f53683f3c489
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2024-06-23 11:41:18 +0100

    Add remaining test case scenarios for multiprices

--- supermarket/src/test/java/com/fluro/exercise/campaigns/MultipricedTest.java
+++ supermarket/src/test/java/com/fluro/exercise/campaigns/MultipricedTest.java
@@ -28,4 +28,42 @@ class MultipricedTest {
         assertFalse(cart.containsKey("B"));
     }
 
+    @Test
+    public void test_CalculateMultipricedNotEnoughQuantity() {
+
+        Map<String, Integer> cart = new HashMap<>();
+        cart.put("A", 1);
+        cart.put("B", 1);
+
+        String sku = "B";
+        int skuQuantity = 2;
+        int multipriceDeal = 125;
+
+        Multipriced multipriced = new Multipriced(sku, skuQuantity, multipriceDeal);
+        int discountedPrice = multipriced.calculateDiscount(cart);
+
+        assertNotEquals(discountedPrice, multipriceDeal);
+        assertTrue(cart.containsKey("B"));
+    }
+
+    @Test
+    public void test_CalculateMultipricedMultipleQuantityAndMore() {
+
+        Map<String, Integer> cart = new HashMap<>();
+        int productQuantityInCart = 5;
+        cart.put("A", 1);
+        cart.put("B", productQuantityInCart);
+
+        String sku = "B";
+        int skuQuantity = 2;
+        int multipriceDeal = 125;
+        int timesDeal = productQuantityInCart / skuQuantity;
+
+        Multipriced multipriced = new Multipriced(sku, skuQuantity, multipriceDeal);
+        int discountedPrice = multipriced.calculateDiscount(cart);
+
+        assertEquals(discountedPrice, timesDeal * multipriceDeal);
+        assertTrue(cart.containsKey("B"));
+    }
+
 }
