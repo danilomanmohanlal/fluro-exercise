d8bde4a25a02be20f318c5471161f386f9f00c72
Danilo Manmohanlal <danilo.manmohanlal@gmail.com>
2024-06-23 11:30:03 +0100

    Add unit test class for multipriced

---
+++ supermarket/src/test/java/com/fluro/exercise/campaigns/MultipricedTest.java
@@ -0,0 +1,31 @@
+package com.fluro.exercise.campaigns;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipricedTest {
+
+
+    @Test
+    public void test_CalculateMultipricedExactQuantity() {
+
+        Map<String, Integer> cart = new HashMap<>();
+        cart.put("A", 1);
+        cart.put("B", 2);
+
+        String sku = "B";
+        int skuQuantity = 2;
+        int multipriceDeal = 125;
+
+        Multipriced multipriced = new Multipriced(sku, skuQuantity, multipriceDeal);
+        int discountedPrice = multipriced.calculateDiscount(cart);
+
+        assertEquals(discountedPrice, multipriceDeal);
+        assertFalse(cart.containsKey("B"));
+    }
+
+}
